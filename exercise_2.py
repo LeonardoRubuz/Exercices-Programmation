2c6f0ec87c01222c559bd67de6e3e26398d713d7
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-08-13 01:39:54 +0100

    alternative function added

--- exercise_2.py
+++ exercise_2.py
@@ -9,4 +9,24 @@ def prix(n:int):
         str(price) + ' centimes'
     )
 
-prix(30)
+#prix(30)
+
+# Alternative : Conversion en dollars dès que le prix dépasse 500 centimes
+def prixDollars(n:int):
+    if n <= 20:
+        price = 10 * n
+    else:
+        price = (10*20) + 8 * (n-20)
+
+    if price > 500:
+        return print(
+            str(n) + ' photocopies vous coûteront :',
+            str(price/100) + ' dollars'
+        )
+    else:
+        return print(
+            str(n) + ' photocopies vous coûteront :',
+            str(price) + ' centimes'
+        )
+    
+prixDollars(60)
