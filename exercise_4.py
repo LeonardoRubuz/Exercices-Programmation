daf2da5635fd0dd507aefbdf7977b02a5f08192e
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-08-13 02:19:43 +0100

    exo4 : divisors number function created

--- exercise_4.py
+++ exercise_4.py
@@ -5,6 +5,15 @@ def triangle(n):
     nombre = 0
     for i in range(n+1):
         nombre += i
-    return print(nombre)
-#triangle(7)
+    return nombre
+#print(triangle(8))
 
+# Fonction nombre de diviseurs
+def nbre_diviseurs(n):
+    compte = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            compte+=1
+    return compte
+
+#print(nbre_diviseurs(7)) 
