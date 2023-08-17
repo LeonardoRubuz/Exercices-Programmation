424120a5421f94c30347c45705ee2f205b2e909b
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-08-17 12:22:17 +0100

    exercise 6 done

---
+++ exercise_6.py
@@ -0,0 +1,16 @@
+# Script 1
+for i in range(3):
+    print('bidule')
+print('truc') # bidule sera affiché trois fois puis truc une seule fois
+
+# Script 2 
+for i in range(3):
+    print('bidule')
+for j in range(4):
+    print('truc') # Bidule sera affiché trois fois et truc quatre fois
+
+# Script 3
+for i in range(3):
+    print('bidule')
+    for j in range(4):
+        print('truc') # bidule sera affiché trois fois et entre chaque bidule, truc sera affiché quatre fois
