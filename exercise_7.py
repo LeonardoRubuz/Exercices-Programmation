5f4a619f90014a7e099c1e946460c39969ec682c
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-08-21 08:49:54 +0100

    exercise 7.1 resolved

---
+++ exercise_7.py
@@ -0,0 +1,7 @@
+def somme_cubes_chiffres(number:int):
+    separatedList = list(str(number))
+    for element in separatedList:
+        separatedList[separatedList.index(element)] = pow(int(element),3)
+    return sum(separatedList)
+
+print(somme_cubes_chiffres(256))
