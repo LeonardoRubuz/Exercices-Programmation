84273fa132dc057fb3dcbcdc6fc4f9f124614587
Arma <141159690+ARIELmakal@users.noreply.github.com>
2023-08-24 20:37:22 +0100

    Add files via upload

---
+++ Exercices-Python-ariel/6.Combien_de_truc_bidule.py
@@ -0,0 +1,40 @@
+#Premier Script 
+'''for i in range (3):
+    print('Bidule')
+print('truc')
+Bidule à l'intérieur de la boucle (For) est affiché 3 et truc une fois
+'''
+
+#Deuxième Script
+'''for i in range(3):
+    print("Bidule")
+for j in range(4):
+    print('Truc')
+
+Bidule à l'intérieur de la boucle (For) est affiché 3 fois
+Truc à l'intérieur de la boucle (For) est affiché 4 fois
+'''
+
+#Troisième Script
+'''for i in range(3):
+    print('Bidule')
+    for j in range(4):
+        print('truc')
+Bidule(1)
+truc(1)
+truc(2)
+truc(3)
+truc(4)
+Bidule(2)
+truc(1)
+truc(2)
+truc(3)
+truc(4)
+Bidule(3)
+truc(1)
+truc(2)
+truc(3)
+truc(4)
+        Fin de la boucle 
+
+'''
